7880dbebcd4a3c05116d4638e8ded7d0dc1755ba
andy <babyandy0111@gmail.com>
2021-04-09 17:30:38 +0800

    oo

--- lib/pages/History/components/Body.dart
+++ lib/pages/History/components/Body.dart
@@ -1,34 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:indochat_officialaccount/layouts/BaseLayoyt.dart';
 import 'package:indochat_officialaccount/theme/size_config.dart';
-import 'package:indochat_officialaccount/layouts/BodyLayout.dart';
-import 'package:indochat_officialaccount/widgets/AppBar.dart';
-
-import 'LeftButtonWidgets.dart';
 import 'MessageBubble.dart';
 
-class Body extends StatelessWidget {
+
+///繼承BaseWidget
+class Body extends BaseLayoyt {
   @override
-  Widget build(BuildContext context) {
-    final String title = 'Channel';
-    return BodyLayout(BodyWidget(title, paddingTop));
+  BodyState getState() => BodyState();
+}
+
+class BodyState extends BaseLayoytState<Body> {
+
+  @override
+  void initState() {
+    super.initState();
+    super.appBarTitle = "History";
   }
-}
-
-class BodyWidget extends StatefulWidget {
-  final String title;
-  final double paddingTop;
-  static bool isSwitched = true;
-
-  BodyWidget(this.title, this.paddingTop) : super();
 
   @override
-  _BodyWidgetState createState() => _BodyWidgetState();
-}
-
-class _BodyWidgetState extends State<BodyWidget> {
-
-  @override
-  Widget build(BuildContext context) {
+  Widget CreatePageView() {
     SizeConfig().init(context);
 
     return SizedBox.expand(
@@ -36,12 +27,6 @@ class _BodyWidgetState extends State<BodyWidget> {
         decoration: BoxDecoration(color: Color.fromRGBO(255, 242, 241, 1)),
         child: Column(
           children: [
-            HeaderBar(
-              widget.title,
-              widget.paddingTop,
-              radiusBarBgColor: Color.fromRGBO(255, 242, 241, 1),
-              leftButtonWidgets: LeftButtonWidgets(),
-            ),
             MessageBubble(),
           ],
         ),
