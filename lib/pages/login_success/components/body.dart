69fdf06846e103f024b53f727fc17285849ec03d
andy <babyandy0111@gmail.com>
2021-04-15 19:45:01 +0800

    整合一下opt到login頁面

--- lib/pages/login_success/components/body.dart
+++ lib/pages/login_success/components/body.dart
@@ -1,37 +1,225 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_cycle/core/shared_preferences/sp.dart';
+import 'package:flutter_cycle/layouts/main-layout.dart';
 import 'package:flutter_cycle/pages/default_demo/default_demo.dart';
+import 'package:flutter_cycle/theme/constants.dart';
 import 'package:flutter_cycle/theme/size_config.dart';
 import 'package:flutter_cycle/widegets/default_button.dart';
+import 'package:flutter_cycle/widegets/form_error.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
 
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  String smscode;
+  final _formKey = GlobalKey<FormState>();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  String _verificationId;
+  final List<String> errors = [];
+
+  void addError({String error}) {
+    if (!errors.contains(error))
+      setState(() {
+        errors.add(error);
+      });
+  }
+
+  void removeError({String error}) {
+    if (errors.contains(error))
+      setState(() {
+        errors.remove(error);
+      });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _auth.setLanguageCode("zh-tw");
+    verifyPhoneNumber();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(height: SizeConfig.screenHeight * 0.04),
-        SizedBox(height: SizeConfig.screenHeight * 0.08),
-        Text(
-          "Login Success",
-          style: TextStyle(
-            fontSize: getProportionateScreenWidth(30),
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
-          ),
-        ),
-        Spacer(),
-        SizedBox(
-          width: SizeConfig.screenWidth * 0.6,
-          child: DefaultButton(
-            text: "Back to home",
-            press: () {
-              // Navigator.pushNamed(context, DefaultDemo.routeName);
-              pushNewScreen(context, screen: DefaultDemo());
-            },
-          ),
-        ),
-        Spacer(),
-      ],
+    return Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            SizedBox(height: SizeConfig.screenHeight * 0.04),
+            SizedBox(height: SizeConfig.screenHeight * 0.08),
+            buildSmsCodeFormField(),
+            SizedBox(height: getProportionateScreenHeight(20)),
+            FormError(errors: errors),
+            // Text(
+            //   "Login Success",
+            //   style: TextStyle(
+            //     fontSize: getProportionateScreenWidth(30),
+            //     fontWeight: FontWeight.bold,
+            //     color: Colors.black,
+            //   ),
+            // ),
+            Spacer(),
+            SizedBox(
+              width: SizeConfig.screenWidth * 0.6,
+              child: DefaultButton(
+                text: "Verify SMS Code",
+                press: () async {
+                  if (_formKey.currentState.validate()) {
+                    _formKey.currentState.save();
+                    bool flag = await signInWithPhoneNumber();
+                    if(flag){
+                      Navigator.pushNamed(context, MainLayout.routeName);
+                    }
+                  }
+                },
+              ),
+            ),
+            Spacer(),
+          ],
+        ));
+  }
+
+  TextFormField buildSmsCodeFormField() {
+    return TextFormField(
+      keyboardType: TextInputType.number,
+      onSaved: (newValue) => smscode = newValue,
+      onChanged: (value) {
+        if (value.isNotEmpty) {
+          removeError(error: ksmsCodeNullError);
+          return "";
+        } else if (value.length < 6) {
+          removeError(error: kShortPincodeError);
+          return "";
+        }
+        return null;
+      },
+      validator: (value) {
+        if (value.isEmpty) {
+          addError(error: ksmsCodeNullError);
+          return "";
+        } else if (value.length < 6) {
+          addError(error: kShortPincodeError);
+          return "";
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        labelText: "sms code",
+        hintText: "Enter your sms code",
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: Icon(Icons.code),
+      ),
     );
+  }
+
+  Future<bool> signInWithPhoneNumber() async {
+    print(smscode);
+    try {
+      final AuthCredential credential = PhoneAuthProvider.credential(
+        verificationId: _verificationId,
+        smsCode: smscode,
+      );
+
+      final User user = (await _auth.signInWithCredential(credential)).user;
+
+      Fluttertoast.showToast(
+          msg: "Successfully signed in UID: ${user.uid}",
+          toastLength: Toast.LENGTH_LONG,
+          gravity: ToastGravity.CENTER,
+          backgroundColor: Colors.green,
+          textColor: Colors.white,
+          fontSize: 16.0);
+      print("Successfully signed in UID: ${user.uid}");
+      return true;
+    } catch (e) {
+      Fluttertoast.showToast(
+          msg: "Failed to sign in: " + e.toString(),
+          toastLength: Toast.LENGTH_LONG,
+          gravity: ToastGravity.CENTER,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0);
+      print("Failed to sign in: " + e.toString());
+      return false;
+    }
+  }
+
+  void verifyPhoneNumber() async {
+    //Callback for when the user has already previously signed in with this phone number on this device
+    PhoneVerificationCompleted verificationCompleted =
+        (PhoneAuthCredential phoneAuthCredential) async {
+      await _auth.signInWithCredential(phoneAuthCredential);
+      print("Phone number automatically verified and user signed in: ${_auth.currentUser.uid}");
+      Fluttertoast.showToast(
+          msg: "Phone number automatically verified and user signed in: ${_auth.currentUser.uid}",
+          toastLength: Toast.LENGTH_LONG,
+          gravity: ToastGravity.CENTER,
+          backgroundColor: Colors.green,
+          textColor: Colors.white,
+          fontSize: 16.0);
+    };
+
+    //Listens for errors with verification, such as too many attempts
+    PhoneVerificationFailed verificationFailed =
+        (FirebaseAuthException authException) {
+      Fluttertoast.showToast(
+          msg: "Phone number verification failed. Code: ${authException.code}. Message: ${authException.message}",
+          toastLength: Toast.LENGTH_LONG,
+          gravity: ToastGravity.CENTER,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0);
+      print('Phone number verification failed. Code: ${authException.code}. Message: ${authException.message}');
+    };
+
+    //Callback for when the code is sent
+    PhoneCodeSent codeSent =
+        (String verificationId, [int forceResendingToken]) async {
+      print('Please check your phone for the verification code.');
+      Fluttertoast.showToast(
+          msg: "Please check your phone for the verification code.",
+          toastLength: Toast.LENGTH_LONG,
+          gravity: ToastGravity.CENTER,
+          backgroundColor: Colors.green,
+          textColor: Colors.white,
+          fontSize: 16.0);
+      _verificationId = verificationId;
+    };
+
+    PhoneCodeAutoRetrievalTimeout codeAutoRetrievalTimeout =
+        (String verificationId) {
+      print("verification code: " + verificationId);
+      Fluttertoast.showToast(
+          msg: "verification code: " + verificationId,
+          toastLength: Toast.LENGTH_LONG,
+          gravity: ToastGravity.CENTER,
+          backgroundColor: Colors.green,
+          textColor: Colors.white,
+          fontSize: 16.0);
+      _verificationId = verificationId;
+    };
+
+    try {
+      await _auth.verifyPhoneNumber(
+          phoneNumber: await SpUtil().getPhone(),
+          timeout: const Duration(seconds: 5),
+          verificationCompleted: verificationCompleted,
+          verificationFailed: verificationFailed,
+          codeSent: codeSent,
+          codeAutoRetrievalTimeout: codeAutoRetrievalTimeout);
+    } catch (e) {
+      print("Failed to Verify Phone Number: ${e}");
+      Fluttertoast.showToast(
+          msg: "Failed to Verify Phone Number: ${e}",
+          toastLength: Toast.LENGTH_LONG,
+          gravity: ToastGravity.CENTER,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0);
+    }
   }
 }
--- lib/pages/sign_in/components/sign_form.dart
+++ lib/pages/sign_in/components/sign_form.dart
@@ -4,6 +4,7 @@ import 'package:flutter_cycle/core/shared_preferences/sp.dart';
 import 'package:flutter_cycle/data/models/response/check-pincode-entity.dart';
 import 'package:flutter_cycle/data/services/user.dart';
 import 'package:flutter_cycle/layouts/main-layout.dart';
+import 'package:flutter_cycle/pages/login_success/login_success.dart';
 import 'package:flutter_cycle/theme/constants.dart';
 import 'package:flutter_cycle/theme/size_config.dart';
 import 'package:flutter_cycle/widegets/default_button.dart';
@@ -19,12 +20,9 @@ class _SignFormState extends State<SignForm> {
   final _formKey = GlobalKey<FormState>();
   String phone;
   String pincode;
-  String smscode;
-  String user_id;
-  bool flag = true;
+  int userId;
 
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  String _verificationId;
+
   final List<String> errors = [];
 
   void addError({String error}) {
@@ -44,7 +42,6 @@ class _SignFormState extends State<SignForm> {
   @override
   void initState() {
     super.initState();
-    _auth.setLanguageCode("zh-tw");
   }
 
   @override
@@ -62,52 +59,45 @@ class _SignFormState extends State<SignForm> {
               Spacer(),
             ],
           ),
-          buildVerifyCodeFormField(),
-          SizedBox(height: getProportionateScreenHeight(20)),
-          FormError(errors: errors),
+          // buildVerifyCodeFormField(),
+          // SizedBox(height: getProportionateScreenHeight(20)),
+          // FormError(errors: errors),
           Row(
             children: [
               Spacer(),
             ],
           ),
-          flag
-              ? DefaultButton(
-                  text: "Get SMS Code",
-                  press: () async {
-                    if (_formKey.currentState.validate()) {
-                      _formKey.currentState.save();
-
-                      // 先判斷有無該indocha帳號
-                      print("$phone /$pincode");
-                      CheckPinCodeEntity data =
-                          await checkPinCode(phone, pincode);
-                      if (data == null) {
-                        addError(error: kNotHasAccount);
-                        return;
-                      }
-
-                      user_id = data.userId.toString();
-
-                      // 如果有該帳號後
-                      // 清掉error 訊息
-                      setState(() {
-                        errors.clear();
-                        // flag = false;
-                      });
-
-                      verifyPhoneNumber();
-                    }
-                  },
-                )
-              : Container(),
           DefaultButton(
-              text: "Verify Code",
-              press: () async {
-                bool push = await signInWithPhoneNumber();
-                if (push) {
-                  Navigator.pushNamed(context, MainLayout.routeName);
+            text: "Get SMS Code",
+            press: () async {
+              if (_formKey.currentState.validate()) {
+                _formKey.currentState.save();
+
+                // 先判斷有無該indocha帳號
+                CheckPinCodeEntity data = await checkPinCode(phone, pincode);
+                if (data == null) {
+                  addError(error: kNotHasAccount);
+                  return;
                 }
-              }),
+
+                userId = data.userId;
+
+                // 如果有該帳號後
+                // 清掉error 訊息
+                setState(() {
+                  errors.clear();
+                });
+
+                // 設定該帳號的一些值
+                await SpUtil().setUserId(userId);
+                await SpUtil().setPinCode(pincode);
+                await SpUtil().setPhone(phone);
+
+                // 推入下一頁
+                Navigator.pushNamed(context, LoginSuccess.routeName);
+              }
+            },
+          ),
         ],
       ),
     );
@@ -172,139 +162,5 @@ class _SignFormState extends State<SignForm> {
         suffixIcon: Icon(Icons.phone),
       ),
     );
-  }
-
-  TextFormField buildVerifyCodeFormField() {
-    return TextFormField(
-      keyboardType: TextInputType.number,
-      // onSaved: (newValue) => smscode = newValue,
-      onChanged: (value) {
-        smscode = value;
-      },
-      decoration: InputDecoration(
-        labelText: "sms code",
-        hintText: "Enter your sms code",
-        floatingLabelBehavior: FloatingLabelBehavior.always,
-        suffixIcon: Icon(Icons.code),
-      ),
-    );
-  }
-
-  Future<bool> signInWithPhoneNumber() async {
-    if (smscode.isEmpty) {
-      addError(error: ksmsCodeNullError);
-      return false;
-    }
-
-    try {
-      final AuthCredential credential = PhoneAuthProvider.credential(
-        verificationId: _verificationId,
-        smsCode: smscode,
-      );
-
-      final User user = (await _auth.signInWithCredential(credential)).user;
-
-      await SpUtil().setPinCode(pincode);
-      await SpUtil().setPhone(phone);
-
-      Fluttertoast.showToast(
-          msg: "Successfully signed in UID: ${user.uid}",
-          toastLength: Toast.LENGTH_LONG,
-          gravity: ToastGravity.CENTER,
-          backgroundColor: Colors.green,
-          textColor: Colors.white,
-          fontSize: 16.0);
-      print("Successfully signed in UID: ${user.uid}");
-      return true;
-    } catch (e) {
-      Fluttertoast.showToast(
-          msg: "Failed to sign in: " + e.toString(),
-          toastLength: Toast.LENGTH_LONG,
-          gravity: ToastGravity.CENTER,
-          backgroundColor: Colors.red,
-          textColor: Colors.white,
-          fontSize: 16.0);
-      print("Failed to sign in: " + e.toString());
-      return false;
-    }
-  }
-
-  void verifyPhoneNumber() async {
-    //Callback for when the user has already previously signed in with this phone number on this device
-    PhoneVerificationCompleted verificationCompleted =
-        (PhoneAuthCredential phoneAuthCredential) async {
-      await _auth.signInWithCredential(phoneAuthCredential);
-      print(
-          "Phone number automatically verified and user signed in: ${_auth.currentUser.uid}");
-      Fluttertoast.showToast(
-          msg:
-              "Phone number automatically verified and user signed in: ${_auth.currentUser.uid}",
-          toastLength: Toast.LENGTH_LONG,
-          gravity: ToastGravity.CENTER,
-          backgroundColor: Colors.green,
-          textColor: Colors.white,
-          fontSize: 16.0);
-    };
-
-    //Listens for errors with verification, such as too many attempts
-    PhoneVerificationFailed verificationFailed =
-        (FirebaseAuthException authException) {
-      Fluttertoast.showToast(
-          msg:
-              "Phone number verification failed. Code: ${authException.code}. Message: ${authException.message}",
-          toastLength: Toast.LENGTH_LONG,
-          gravity: ToastGravity.CENTER,
-          backgroundColor: Colors.red,
-          textColor: Colors.white,
-          fontSize: 16.0);
-      print(
-          'Phone number verification failed. Code: ${authException.code}. Message: ${authException.message}');
-    };
-
-    //Callback for when the code is sent
-    PhoneCodeSent codeSent =
-        (String verificationId, [int forceResendingToken]) async {
-      print('Please check your phone for the verification code.');
-      Fluttertoast.showToast(
-          msg: "Please check your phone for the verification code.",
-          toastLength: Toast.LENGTH_LONG,
-          gravity: ToastGravity.CENTER,
-          backgroundColor: Colors.green,
-          textColor: Colors.white,
-          fontSize: 16.0);
-      _verificationId = verificationId;
-    };
-
-    PhoneCodeAutoRetrievalTimeout codeAutoRetrievalTimeout =
-        (String verificationId) {
-      print("verification code: " + verificationId);
-      Fluttertoast.showToast(
-          msg: "verification code: " + verificationId,
-          toastLength: Toast.LENGTH_LONG,
-          gravity: ToastGravity.CENTER,
-          backgroundColor: Colors.green,
-          textColor: Colors.white,
-          fontSize: 16.0);
-      _verificationId = verificationId;
-    };
-
-    try {
-      await _auth.verifyPhoneNumber(
-          phoneNumber: phone,
-          timeout: const Duration(seconds: 5),
-          verificationCompleted: verificationCompleted,
-          verificationFailed: verificationFailed,
-          codeSent: codeSent,
-          codeAutoRetrievalTimeout: codeAutoRetrievalTimeout);
-    } catch (e) {
-      print("Failed to Verify Phone Number: ${e}");
-      Fluttertoast.showToast(
-          msg: "Failed to Verify Phone Number: ${e}",
-          toastLength: Toast.LENGTH_LONG,
-          gravity: ToastGravity.CENTER,
-          backgroundColor: Colors.red,
-          textColor: Colors.white,
-          fontSize: 16.0);
-    }
   }
 }
