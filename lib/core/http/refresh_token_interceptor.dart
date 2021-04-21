4581e8cc6086bf50778b1edee22b7bcd3b758569
andy <babyandy0111@gmail.com>
2021-04-21 11:01:07 +0800

    fix token

--- lib/core/http/refresh_token_interceptor.dart
+++ lib/core/http/refresh_token_interceptor.dart
@@ -5,7 +5,7 @@ import 'package:dio_retry_fixed/dio_retry_fixed.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_cycle/data/models/request/token_post_entity.dart';
 import 'package:flutter_cycle/data/models/response/token_entity.dart';
-import 'package:flutter_cycle/data/repositorys/token_repository.dart';
+import 'package:flutter_cycle/data/services/token.dart';
 import '../shared_preferences/sp.dart';
 import 'api_response.dart';
 
@@ -22,10 +22,9 @@ class RefreshTokenInterceptor extends Interceptor {
   Future onError(DioError err) async {
     var extra = RetryOptions.fromExtra(err.request) ?? this.options;
     var shouldRetry = extra.retries > 0 && await options.retryEvaluator(err);
-    int userid = await SpUtil().getUserId();
+    String oldtoken = await SpUtil().getToken();
 
-    print(userid);
-    if (shouldRetry && userid != null) {
+    if (shouldRetry && oldtoken != null) {
       if (extra.retryInterval.inMilliseconds > 0) {
         await Future.delayed(extra.retryInterval);
       }
@@ -35,20 +34,20 @@ class RefreshTokenInterceptor extends Interceptor {
       err.request.extra = err.request.extra..addAll(extra.toExtra());
       RequestOptions ops = err.response.request;
 
-      String nowToken = await SpUtil().getToken();
-      if (nowToken != ops.headers["Bearer"]) {
-        ops.headers["Bearer"] = nowToken;
-        //repeat
-        return dio.request(
-          err.request.path,
-          cancelToken: err.request.cancelToken,
-          data: err.request.data,
-          onReceiveProgress: err.request.onReceiveProgress,
-          onSendProgress: err.request.onSendProgress,
-          queryParameters: err.request.queryParameters,
-          options: err.request,
-        );
-      }
+      // 這裡好像可以不需要, 先拿掉
+      // if (oldtoken != ops.headers["Bearer"]) {
+      //   ops.headers["Bearer"] = oldtoken;
+      //   //repeat
+      //   return dio.request(
+      //     err.request.path,
+      //     cancelToken: err.request.cancelToken,
+      //     data: err.request.data,
+      //     onReceiveProgress: err.request.onReceiveProgress,
+      //     onSendProgress: err.request.onSendProgress,
+      //     queryParameters: err.request.queryParameters,
+      //     options: err.request,
+      //   );
+      // }
 
       if (err.response?.statusCode == 401) {
         print("鎖");
@@ -57,11 +56,9 @@ class RefreshTokenInterceptor extends Interceptor {
         dio.interceptors.responseLock.lock();
         dio.interceptors.errorLock.lock();
 
-        ApiResponse<TokenEntity> entity = await TokenRepository.refreshToken();
-        var newToken = entity.data.token;
-        await SpUtil().setToken(newToken);
-        ops.headers["Bearer"] = newToken;
-        print("換好 token: ${newToken}");
+        TokenEntity entity = await refreshToken();
+        ops.headers["Bearer"] = entity.token;
+        print("換好 token: ${entity.token}");
 
         print("解鎖");
         dio.unlock();
